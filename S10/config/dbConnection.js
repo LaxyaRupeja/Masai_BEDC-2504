5f1766e6d2b033def5ef363f3801d4842afe3a18
Laxya Rupeja <159245983+Laxya-KTH@users.noreply.github.com>
2025-04-29 21:55:03 +0530

    Session 10: Fixed

--- S10/config/dbConnection.js
+++ S10/config/dbConnection.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://ishaan:lFm5zd35sJUkQQLl@test.j6qmfgv.mongodb.net/lms",{
-            
-        });
+        await mongoose.connect("mongodb://localhost:27017/lms")
     } catch (error) {
         console.log(error);
         console.log("Something went wrong in database connection");
